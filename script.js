f8880896085a2d42393c2de390ace87a31b7487f
Ken Camarador <ken-camarador@hotmail.com>
2023-04-17 23:34:07 -0400

    added twitter project

--- script.js
+++ script.js
@@ -12,23 +12,26 @@ var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
 var span4 = document.getElementsByClassName("close4")[0];
+var span5 = document.getElementsByClassName("close5")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-}
+  }
 
 btn2.onclick = function() {
     modal2.style.display = "block";
@@ -40,7 +43,11 @@ btn3.onclick = function() {
   
 btn4.onclick = function() {
       modal4.style.display = "block";
-    }
+  }
+
+btn5.onclick = function() {
+    modal5.style.display = "block";
+  }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -59,6 +66,10 @@ span4.onclick = function() {
       modal4.style.display = "none";
     }
 
+span5.onclick = function() {
+      modal5.style.display = "none";
+    }
+
 
     window.onclick = function(event) {
         if (event.target == modal) {
@@ -72,4 +83,8 @@ span4.onclick = function() {
           else if (event.target == modal4) {
           modal4.style.display = "none";
           }
+          else if (event.target == modal5) {
+          modal5.style.display = "none";
+            }
+          
       };
